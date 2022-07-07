3c09a2ce203682b0b5fb46da001ec34ec7101410
Lukas Ewecker <lukasewecker@web.de>
2022-07-07 13:44:07 +0200

    small refactor

--- src/detect.py
+++ src/detect.py
@@ -65,15 +65,8 @@ class Yolov6Publisher:
             fed into the yolo network. Final output coordinates will be rescaled to
             the original img size.
         """
-        self._init_complete = False
         self.img_size = img_size
 
-        self.img_subscriber = rospy.Subscriber(
-            img_topic, Image, self.process_img_msg
-        )
-        self.detection_publisher = rospy.Publisher(
-            pub_topic, Detection2DArray, queue_size=queue_size
-        )
         vis_topic = pub_topic + "visualization" if pub_topic.endswith("/") else \
             pub_topic + "/visualization"
         self.visualization_publisher = rospy.Publisher(
@@ -87,56 +80,59 @@ class Yolov6Publisher:
             weights=weights, conf_thresh=conf_thresh, iou_thresh=iou_thresh,
             device=device
         )
-        self._init_complete = True
+        self.img_subscriber = rospy.Subscriber(
+            img_topic, Image, self.process_img_msg
+        )
+        self.detection_publisher = rospy.Publisher(
+            pub_topic, Detection2DArray, queue_size=queue_size
+        )
 
     def process_img_msg(self, img_msg: Image):
         """ callback function for publisher """
-        if self._init_complete:
-            np_img_orig = self.bridge.imgmsg_to_cv2(
-                img_msg, desired_encoding='passthrough'
-            )
+        np_img_orig = self.bridge.imgmsg_to_cv2(
+            img_msg, desired_encoding='passthrough'
+        )
 
-            # handle possible different img formats
-            if len(np_img_orig.shape) == 2:
-                np_img_orig = np.stack([np_img_orig] * 3, axis=2)
+        # handle possible different img formats
+        if len(np_img_orig.shape) == 2:
+            np_img_orig = np.stack([np_img_orig] * 3, axis=2)
 
-            h_orig, w_orig, c = np_img_orig.shape
-            if c == 1:
-                np_img_orig = np.concatenate([np_img_orig] * 3, axis=2)
-                c = 3
+        h_orig, w_orig, c = np_img_orig.shape
+        if c == 1:
+            np_img_orig = np.concatenate([np_img_orig] * 3, axis=2)
+            c = 3
 
-            # automatically resize the image to the next smaller possible size
-            w_scaled, h_scaled = self.img_size
+        # automatically resize the image to the next smaller possible size
+        w_scaled, h_scaled = self.img_size
 
-            # w_scaled = w_orig - (w_orig % 8)
-            np_img_resized = cv2.resize(np_img_orig, (w_scaled, h_scaled))
+        # w_scaled = w_orig - (w_orig % 8)
+        np_img_resized = cv2.resize(np_img_orig, (w_scaled, h_scaled))
 
-            # conversion to torch tensor (copied from original yolov6 repo)
-            img = np_img_resized.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
-            img = torch.from_numpy(np.ascontiguousarray(img))
-            img = img.float()  # uint8 to fp16/32
-            img /= 255  # 0 - 255 to 0.0 - 1.
-            img = img.to(device)
+        # conversion to torch tensor (copied from original yolov6 repo)
+        img = np_img_resized.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        img = torch.from_numpy(np.ascontiguousarray(img))
+        img = img.float()  # uint8 to fp16/32
+        img /= 255  # 0 - 255 to 0.0 - 1.
+        img = img.to(device)
 
-            # inference & rescaling the output to original img size
-            detections = self.model.inference(img)
-            detections[:, :4] = Inferer.rescale(
-                [h_scaled, w_scaled], detections[:, :4], [h_orig, w_orig])
-            detections[:, :4] = detections[:, :4].round()
+        # inference & rescaling the output to original img size
+        detections = self.model.inference(img)
+        detections[:, :4] = Inferer.rescale(
+            [h_scaled, w_scaled], detections[:, :4], [h_orig, w_orig])
+        detections[:, :4] = detections[:, :4].round()
 
-            # publishing
-            detection_msg = create_detection_msg(img_msg, detections)
-            self.detection_publisher.publish(detection_msg)
+        # publishing
+        detection_msg = create_detection_msg(img_msg, detections)
+        self.detection_publisher.publish(detection_msg)
 
-            # visualizing if required
-            if self.visualization_publisher:
-                bboxes = [[int(x1), int(y1), int(x2), int(y2)] for x1, y1, x2,
-                                                            y2 in detections[:,
-                                                                  :4].tolist()]
-                classes = [int(c) for c in detections[:, 5].tolist()]
-                vis_img = draw_detections(np_img_orig, bboxes, classes)
-                vis_msg = self.bridge.cv2_to_imgmsg(vis_img)
-                self.visualization_publisher.publish(vis_msg)
+        # visualizing if required
+        if self.visualization_publisher:
+            bboxes = [[int(x1), int(y1), int(x2), int(y2)]
+                      for x1, y1, x2, y2 in detections[:, :4].tolist()]
+            classes = [int(c) for c in detections[:, 5].tolist()]
+            vis_img = draw_detections(np_img_orig, bboxes, classes)
+            vis_msg = self.bridge.cv2_to_imgmsg(vis_img)
+            self.visualization_publisher.publish(vis_msg)
 
 
 if __name__ == "__main__":
